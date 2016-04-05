a176ded2a678d1472d1713ec789b306b11d2c585
Fred Yankowski <fcy@imsa.edu>
2016-04-05 17:09:24 -0500

    Rework to use ReadCloser like the examples in elazrl/goproxy.

--- main.go
+++ main.go
@@ -1,42 +1,63 @@
 package main
 
 import (
-	"bytes"
 	"flag"
+	"fmt"
 	"github.com/elazarl/goproxy"
-	_ "io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"regexp"
 )
 
+const (
+	REQ  string = "request"
+	RESP        = "response"
+)
+
 type BufferCloser struct {
-	bytes.Buffer
+	tipe string
+	Id   string
+	R    io.ReadCloser
 }
-func (b BufferCloser) Close() error {
-	return nil
+
+func (c *BufferCloser) Read(b []byte) (n int, err error) {
+	n, err = c.R.Read(b)
+	log.Printf("%s %s n=%d err=%v", c.tipe, c.Id, n, err)
+	if n > 0 {
+		sep := "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n"
+		log.Printf("%s %s body=%s"+sep, c.tipe, c.Id, string(b[:n]))
+	}
+	return n, err
+}
+
+func (c BufferCloser) Close() error {
+	log.Printf("Close: %s %s\n", c.tipe, c.Id)
+	return c.R.Close()
 }
 
 // HTTP/HTTPS proxy for debugging
 func main() {
 	addr := flag.String("addr", ":8080", "proxy listen address")
-	hostmatch := flag.String("hostmatch", ".*", "hosts to trace (regexp pattern)")
+	hostmatch := flag.String("hostmatch", "^.*$", "hosts to trace (regexp pattern)")
 	verbose := flag.Bool("v", false, "verbose output")
 	flag.Parse()
+
 	proxy := goproxy.NewProxyHttpServer()
+
 	proxy.OnRequest(goproxy.ReqHostMatches(regexp.MustCompile(*hostmatch))).
 		HandleConnect(goproxy.AlwaysMitm)
-	proxy.OnRequest().DoFunc(func (req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-		log.Println(req.Method, req.URL)
-		var b BufferCloser
-		n, err := b.ReadFrom(req.Body)
-		if err != nil {
-			log.Println(err)
-		} else {
-			log.Printf("request body (%s): %s\n", n, b.String())
-			req.Body = b
-		}
+
+	proxy.OnRequest().DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		id := fmt.Sprintf("%v %v", req.Method, req.URL)
+		req.Body = &BufferCloser{REQ, id, req.Body}
 		return req, nil
+	})
+
+	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+		id := fmt.Sprintf("%v", ctx.Req.URL.String())
+		resp.Body = &BufferCloser{RESP, id, resp.Body}
+		return resp
 	})
 
 	proxy.Verbose = *verbose
